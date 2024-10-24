7b2c7660da038e01c810f3aced79148164e4fbd4
Hamish Croser <h.croser1@gmail.com>
2024-10-24 11:43:05 +1100

    Added boilerplate files

---
+++ atap_corpus_timeline/CorpusTimeline.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from atap_corpus_loader import CorpusLoader
+
+
+class CorpusTimeline:
+    def __init__(self,
+                 corpus_loader: Optional[CorpusLoader] = None,
+                 run_logger: bool = False,
+                 **params):
+        pass
