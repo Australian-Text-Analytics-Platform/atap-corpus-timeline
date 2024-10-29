4f691599e2f3cc397640c376d8f344a357c20162
Hamish Croser <h.croser1@gmail.com>
2024-10-30 09:17:27 +1100

    Added overall document frequency plot

--- atap_corpus_timeline/CorpusVisualiser.py
+++ atap_corpus_timeline/CorpusVisualiser.py
@@ -21,6 +21,7 @@ class CorpusVisualiser(pn.viewable.Viewer):
         'weeks': 'W', 'days': 'D', 'hours': 'h',
         'minutes': 'min', 'seconds': 's'
     }
+    TIME_PERIOD_INVERSE_GROUPINGS: dict[str, str] = {v: k for k, v in TIME_PERIOD_GROUPINGS.items()}
 
     CONTROLS_MAX_WIDTH: int = 120
 
@@ -73,34 +74,51 @@ class CorpusVisualiser(pn.viewable.Viewer):
         return self.panel.servable()
 
     def _update_corpus_list(self, *_):
-        corpus_options: dict[str, DataFrameCorpus] = self.corpus_loader.get_corpora()
-        self.corpus_selector.options = corpus_options
-        if len(corpus_options):
-            self.corpus_selector.value = list(corpus_options.values())[-1]
+        self.log("Updated corpus list", logging.DEBUG)
+        try:
+            corpus_options: dict[str, DataFrameCorpus] = self.corpus_loader.get_corpora()
+            if self.corpus_selector.options != corpus_options:
+                self.corpus_selector.options = corpus_options
+                if len(corpus_options):
+                    self.corpus_selector.value = list(corpus_options.values())[-1]
+        except Exception as e:
+            self.log(str(traceback.format_exc()), logging.DEBUG)
 
     def _update_selected_corpus(self, *_):
-        corpus: Optional[DataFrameCorpus] = self.corpus_selector.value
-        if corpus is None:
-            self.time_col_selector.value = None
-            self.included_meta_selector.value = None
-        else:
-            meta_list: list[str] = corpus.metas
-            self.included_meta_selector.options = [corpus._COL_DOC] + meta_list
-            datetime_metas: list[str] = [col for col in meta_list if is_datetime64_any_dtype(corpus.get_meta(col))]
-            self.time_col_selector.options = datetime_metas
-            if len(datetime_metas):
-                self.time_col_selector.value = datetime_metas[0]
+        self.log("Updated selected corpus", logging.DEBUG)
+        try:
+            corpus: Optional[DataFrameCorpus] = self.corpus_selector.value
+            if corpus is None:
+                self.time_col_selector.value = None
+                self.included_meta_selector.value = None
+            else:
+                meta_list: list[str] = corpus.metas
+                selectable_list: list[str] = meta_list + [corpus._COL_DOC]
+                if self.included_meta_selector.options != selectable_list:
+                    self.included_meta_selector.options = selectable_list
+                    self.included_meta_selector.value = selectable_list[0]
+                datetime_metas: list[str] = [col for col in meta_list if is_datetime64_any_dtype(corpus.get_meta(col))]
+                if self.time_col_selector.options != datetime_metas:
+                    self.time_col_selector.options = datetime_metas
+                    if len(datetime_metas):
+                        self.time_col_selector.value = datetime_metas[0]
+        except Exception as e:
+            self.log(str(traceback.format_exc()), logging.DEBUG)
 
     def _update_time_column(self, *_):
-        corpus: Optional[DataFrameCorpus] = self.corpus_selector.value
-        time_col: Optional[str] = self.time_col_selector.value
-        if (corpus is None) or (time_col is None) or (time_col not in corpus.metas):
-            self.date_range_picker.value = None
-        else:
-            date_series: Series = corpus.get_meta(time_col)
-            self.date_range_picker.start = date_series.min()
-            self.date_range_picker.end = date_series.max()
-            self.date_range_picker.value = (self.date_range_picker.start, self.date_range_picker.end)
+        self.log("Updated time column", logging.DEBUG)
+        try:
+            corpus: Optional[DataFrameCorpus] = self.corpus_selector.value
+            time_col: Optional[str] = self.time_col_selector.value
+            if (corpus is None) or (time_col is None) or (time_col not in corpus.metas):
+                self.date_range_picker.value = None
+            else:
+                date_series: Series = corpus.get_meta(time_col)
+                self.date_range_picker.start = date_series.min()
+                self.date_range_picker.end = date_series.max()
+                self.date_range_picker.value = (self.date_range_picker.start, self.date_range_picker.end)
+        except Exception as e:
+            self.log(str(traceback.format_exc()), logging.DEBUG)
 
     def generate_plots(self, *_):
         try:
@@ -112,28 +130,49 @@ class CorpusVisualiser(pn.viewable.Viewer):
             time_col: str = self.time_col_selector.value
             meta_col: str = self.included_meta_selector.value
 
+            start_date, end_date = self.date_range_picker.value
+            mask = (plot_df[time_col] >= start_date) & (plot_df[time_col] <= end_date)
+            filtered_df = plot_df.loc[mask]
+
             plots = [
-                self.create_frequency_plot(plot_df, time_col, meta_col)
+                self.create_raw_frequency_plot(filtered_df, time_col),
+                self.create_meta_frequency_plot(filtered_df, time_col, meta_col)
             ]
 
             self.plots.objects = plots
         except Exception as e:
             self.log(str(traceback.format_exc()), logging.DEBUG)
 
-    def create_frequency_plot(self, plot_df: DataFrame, time_col: str, meta_col: str) -> Plotly:
-        self.log(f"Plot_df: {plot_df.shape}, time_col: {time_col}, meta_col: {meta_col}", logging.DEBUG)
-        start_date, end_date = self.date_range_picker.value
-
-        mask = (plot_df[time_col] >= start_date) & (plot_df[time_col] <= end_date)
-        filtered_df = plot_df.loc[mask]
+    def create_raw_frequency_plot(self, plot_df: DataFrame, time_col: str) -> Plotly:
+        self.log(f"create_raw_frequency_plot method: Plot_df: {plot_df.shape}, time_col: {time_col}", logging.DEBUG)
 
         date_grouping_periods = self.date_group_periods.value
         date_group_unit = self.date_group_unit_selector.value
         frequency: str = str(date_grouping_periods) + date_group_unit
-        count_col_name: str = f'{meta_col}_frequency'
-        grouped_df = filtered_df.groupby([Grouper(key=time_col, freq=frequency), meta_col]).size().reset_index(name=count_col_name)
+        count_col_name: str = 'Document count'
+        grouped_df = plot_df.groupby([Grouper(key=time_col, freq=frequency)]).size().reset_index(name=count_col_name)
 
-        fig = px.line(grouped_df, x=time_col, y=count_col_name, color=meta_col, title=f"{meta_col} Frequency by {frequency} Intervals")
+        date_group_readable = CorpusVisualiser.TIME_PERIOD_INVERSE_GROUPINGS[date_group_unit]
+        fig = px.line(grouped_df, x=time_col, y=count_col_name,
+                      title=f"Document count by {date_group_readable} intervals")
+        fig.update_traces(mode="lines+markers", marker=dict(size=10), line=dict(width=4))
+
+        return Plotly(fig)
+
+    def create_meta_frequency_plot(self, plot_df: DataFrame, time_col: str, meta_col: str) -> Plotly:
+        """
+        Creates a line plot of time by frequency within the specified meta column
+        """
+        self.log(f"create_meta_frequency_plot method: Plot_df: {plot_df.shape}, time_col: {time_col}, meta_col: {meta_col}", logging.DEBUG)
+
+        date_grouping_periods = self.date_group_periods.value
+        date_group_unit = self.date_group_unit_selector.value
+        frequency: str = str(date_grouping_periods) + date_group_unit
+        count_col_name: str = f'{meta_col} frequency'
+        grouped_df = plot_df.groupby([Grouper(key=time_col, freq=frequency), meta_col]).size().reset_index(name=count_col_name)
+
+        date_group_readable = CorpusVisualiser.TIME_PERIOD_INVERSE_GROUPINGS[date_group_unit]
+        fig = px.line(grouped_df, x=time_col, y=count_col_name, color=meta_col, title=f"{meta_col} frequency breakdown by {date_group_readable} intervals")
         fig.update_traces(mode="lines+markers", marker=dict(size=10), line=dict(width=4))
 
         return Plotly(fig)
